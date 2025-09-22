d113646fc84bbd34adb0a2ad9419ed2c6c939845
Karl Podger <karl.podger@primeordinal.com>
2025-09-22 12:14:35 +0100

    chore: Add @vercel/speed-insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
@@ -31,6 +32,7 @@ export default function RootLayout({
       >
         <Providers>{children}</Providers>
       </body>
+      <SpeedInsights/>
     </html>
   );
 }
